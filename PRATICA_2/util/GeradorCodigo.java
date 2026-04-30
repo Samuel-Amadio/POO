2ff3d9539845e256bd877cd3a1f7c9e6a1c75ce7
Samuel-Amadio <amadiosamuel06@gmail.com>
2026-04-29 23:56:46 -0300

    Update GeradorCodigo.java

--- PRATICA_2/util/GeradorCodigo.java
+++ PRATICA_2/util/GeradorCodigo.java
@@ -1,5 +1,14 @@
 package util;
 
 public class GeradorCodigo {
-
+	
+	private static int sequencial = 1;
+	
+	public static String gerarCodigo(String prefixo) {
+		String codigo = String.format("%s-%03d", prefixo.toUpperCase(), sequencial);
+		sequencial++;
+		return codigo;
+	}
 }
+
+
